e830cb2a85f9bd9b091b09023f7dadd8da4220fd
Rajat-Dub3y <raja1dub3y@gmail.com>
2026-02-19 20:48:31 +0530

    fixed cors issue1

--- model/API/api.py
+++ model/API/api.py
@@ -11,13 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://phishingtester.vercel.app"
-    ],  # Vite ports
+    allow_origins="*",  # Vite ports
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
